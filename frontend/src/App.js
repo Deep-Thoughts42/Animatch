e3481b24389ca7f3cb7cdbe5c56dc2b625fd7d97
Vedaant <vedaant.varshney@gmail.com>
2021-07-10 17:26:24 -0400

    Updated GUI and Timer Information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './images/logo.svg';
 import './css/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Row, Col, Image } from 'react-bootstrap';
+import { Button, Container, Row, Col, Image, ProgressBar } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 // Component Imports
 import SmallScreenContext from './components/SmallScreenContext';
+import ScoreContext from './components/ScoreContext';
+import MiscContext from './components/MiscContext';
 import AnswerButton from './components/AnswerButton';
 import axios from 'axios';
 
@@ -39,82 +41,136 @@ function App() {
   const [options, setOptions] = useState([])
   const [answer, setAnswer] = useState("")
 
+  let [score, setScore] = useState(0);
+  let scoreArray = [10, 7, 5, 3, 2, 1]
+  let [scoreEarned, setScoreEarned] = useState(10)
+
+
+  let [userLogMessage, setUserLogMessage] = useState("");
+
+
 
 
   const [imageIdx, setImageIdx] = useState(0)
+  const [timerBar, setTimerBar] = useState(100)
+
+
+  function timerEvent() {
+
+    setInterval(() => {
+      let time_left = 12
+      let progress_bar = timerBar
+      progress_bar = ((time_left)/12)*100
+      time_left = time_left-0.1
+      setTimerBar(progress_bar)
+      console.log(progress_bar)
+
+    }, 100)
+  }
 
   function handleTimer() {
-    let idx = 1;
+    let idx = 0;
 
     let changeInterval = setInterval(() => {
       setImageIdx(idx)
+      setScoreEarned(scoreArray[idx])
       idx++;
+
       if (idx > 5) {
         clearInterval(changeInterval)
       }
-      
-    }, 1000)
+
+    }, 2000)
 
   }
 
   function handleRequest() {
     axios({
       method: 'get',
-      url: 'https://vedaantv.pythonanywhere.com/generate',  
+      url: 'https://vedaantv.pythonanywhere.com/generate',
     })
       .then((res) => {
         // console.log(res.data);
+        setImageIdx(0)
         setImageStrings(res.data.image_strings)
         setAnswer(res.data.answer)
         setOptions(res.data.options)
+        // timerEvent()
       })
       .catch((err) => {
-        
+
         console.log(err)
       });
 
   }
 
-
+  function handleRestart() {
+    setScore(0)
+    setScoreEarned(10)
+    handleRequest()
+    setUserLogMessage("The application is now restarting.")
+    setTimeout(() => {
+      setUserLogMessage("")
+    }, 5000)
+  }
 
 
 
 
   return (
     <div className="">
-      <SmallScreenContext.Provider value={smallScreen}>
-        <Container fluid='lg' className="c-w change-font mt-5 ">
-          <Row className="text-center">
-            <h1>Welcome to <span className="fw-700 text-blue">Who's that Character!</span></h1>
-            <h3>Your goal is to correctly identify as many characters as possible. Good Luck!</h3>
-          </Row>
-          <Row className=" mt-5">
-            <Col lg="6" className="text-center d-flex flex-column align-items-center">
-              <h4 className="mb-3">Who is this character?</h4>
-              <Image rounded src={`data:image/jpg;base64,${imageStrings[imageIdx]}`} className={smallScreen ? "mb-4 dims-small" : "mb-4 dims-rel"} />
-              <h4>Score: <span className="text-blue fw-700">0</span></h4>
-              <Button onClick={handleTimer}>Test Timer</Button>
+      <MiscContext.Provider value={[imageIdx, setImageIdx, handleRequest, userLogMessage, setUserLogMessage, scoreEarned, setScoreEarned]}>
+        <ScoreContext.Provider value={[score, setScore]}>
+          <SmallScreenContext.Provider value={smallScreen}>
+            <Container fluid='lg' className="c-w change-font mt-5 mb-5">
+              <Row className="text-center">
+                <h1>Welcome to <span className="fw-700 text-blue">Who's that Character!</span></h1>
+                <h3>Your goal is to correctly identify as many characters as possible. Good Luck!</h3>
+              </Row>
 
-            </Col>
-            <Col lg="6" className="mt-3 text-center d-flex flex-column align-items-center ">
-              <div className="align-self-center">
-                <AnswerButton name={options[0]} correct={answer} />
-                <AnswerButton name={options[1]} correct={answer}/>
-                <AnswerButton name={options[2]} correct={answer}/>
-                <AnswerButton name={options[3]} correct={answer}/>
-
-              </div>
-              <Button onClick={handleRequest}>Test</Button>
-              
-
-            </Col>
-          </Row>
-
-        </Container>
+              <Row className=" mt-5">
+                <Col lg="6" className="text-center d-flex flex-column align-items-center">
+                  <h4 className="mb-3">Who is this character?</h4>
+                  <Image rounded src={`data:image/jpg;base64,${imageStrings[imageIdx]}`} className={smallScreen ? "mb-4 dims-small" : "mb-4 dims-rel"} />
+                  
 
 
+                </Col>
 
-      </SmallScreenContext.Provider>
+                <Col lg="6" className="mt-3 text-center d-flex flex-column align-items-center ">
+                  <div className="align-self-center d-flex justify-content-center flex-column">
+                    <AnswerButton name={options[0]} correct={answer} />
+                    <AnswerButton name={options[1]} correct={answer} />
+                    <AnswerButton name={options[2]} correct={answer} />
+                    <AnswerButton name={options[3]} correct={answer} />
+                    <Button onClick={handleRestart} className={smallScreen ? "mt-3 restart-button-small w-100" : "mt-3 restart-button w-100"}><h3 style={{ "margin": 0 }}>Restart</h3></Button>
+                    <ProgressBar className="mt-5 mb-3" now={30.9999} />
+                    <h4>You will currently earn <span className="text-blue fw-700">{scoreEarned}</span> points!</h4>
+                  <h4>Score: <span className="text-blue fw-700">{score}</span></h4>
+
+                  <Button onClick={handleTimer}>Test Timer</Button>
+
+
+                  </div>
+
+                  <h3 className="mt-3">{userLogMessage}</h3>
+
+                </Col>
+
+
+
+
+              </Row>
+
+            </Container>
+
+
+
+          </SmallScreenContext.Provider>
+
+        </ScoreContext.Provider>
+
+      </MiscContext.Provider>
 
     </div>
   );
--- frontend/src/components/AnswerButton.js
+++ frontend/src/components/AnswerButton.js
@@ -2,6 +2,8 @@ import { React, useState, useContext } from 'react';
 import { Button } from 'react-bootstrap';
 import '../css/app.css'
 import SmallScreenContext from './SmallScreenContext';
+import ScoreContext from './ScoreContext';
+import MiscContext from './MiscContext';
 import axios from 'axios';
 
 function AnswerButton(props) {
@@ -9,16 +11,48 @@ function AnswerButton(props) {
     // function space
 
     const smallScreen = useContext(SmallScreenContext)
+    const [score, setScore] = useContext(ScoreContext)
+    const [imageIdx, setImageIdx, handleRequest, userLogMessage, setUserLogMessage, scoreEarned, setScoreEarned] = useContext(MiscContext)
+
+    function handleClick() {
+        if (props.name === props.correct) {
+            console.log("Your Answer was correct")
+            setScore(score + scoreEarned)
+            handleRequest()
+            setUserLogMessage("Your answer was correct, good job!")
+            setTimeout(() => {
+                setUserLogMessage("")
+            }, 5000)
+            setScoreEarned(10)
+
+        }
+        else {
+            console.log("Your answer was incorrect, the correct answer was " + props.correct)
+            setScore(0)
+            setImageIdx(5)
+            handleRequest()
+            setUserLogMessage("Your answer was incorrect, the correct answer was " + props.correct + ". The game is restarting.")
+            setTimeout(() => {
+                setUserLogMessage("")
+            }, 5000)
+            setScoreEarned(10)
+
+        }
+    }
+
+    
+
+
 
 
 
 
     return (
-        <div className="mb-3">
-            <Button className={smallScreen ? "button-override-small d-flex justify-content-center": "button-override d-flex align-items-center justify-content-center"}><h3 style={{"margin": 0}}>{props.name}</h3></Button>
+        <div className="mb-3 d-flex justify-content-center">
+            <Button onClick={handleClick} className={smallScreen ? "button-override-small d-flex justify-self-center justify-content-center" : "button-override d-flex align-items-center justify-content-center justify-self-center"}><h3 style={{ "margin": 0 }}>{props.name}</h3></Button>
         </div>
 
-        
+
     )
 
 
