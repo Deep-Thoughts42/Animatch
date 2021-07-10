eca67b6d6272c3c5110ca5d999cd0a75e3ce0930
Vedaant <vedaant.varshney@gmail.com>
2021-07-10 14:24:17 -0400

    Updating Hosted Files + React

--- frontend/src/components/AnswerButton.js
+++ frontend/src/components/AnswerButton.js
@@ -2,12 +2,14 @@ import { React, useState, useContext } from 'react';
 import { Button } from 'react-bootstrap';
 import '../css/app.css'
 import SmallScreenContext from './SmallScreenContext';
+import axios from 'axios';
 
 function AnswerButton(props) {
 
     // function space
 
     const smallScreen = useContext(SmallScreenContext)
+
 
 
 
