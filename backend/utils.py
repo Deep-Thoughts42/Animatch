cf34ba880649a5c1f1929dbcf80c6fbf09186b4c
TSharvananthan <thanussian@gmail.com>
2021-07-10 12:55:06 -0400

    created create_base64_strings function

--- backend/utils.py
+++ backend/utils.py
@@ -1,7 +1,21 @@
+import base64
 import os
 import json
 
 def create_character_sheet():
     open("character_sheet.json", "w").write(json.dumps(os.listdir("db")))
 
-create_character_sheet()
+def encode_image_base64(image_path: str):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+
+    return encoded_string
+
+def create_base64_strings(character_name: str):
+    directory = f"db/{character_name}"
+
+    os.chdir(directory)
+
+    files_to_convert = [encode_image_base64(file) for file in os.listdir()]
+
+    return files_to_convert
