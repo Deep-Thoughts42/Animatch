fac76d9dd3ed0e11ef23fb0a460371cbb4e642d6
Vedaant <vedaant.varshney@gmail.com>
2021-07-10 15:14:00 -0400

    Updated API Calls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 // Component Imports
 import SmallScreenContext from './components/SmallScreenContext';
 import AnswerButton from './components/AnswerButton';
+import axios from 'axios';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
     if (window.innerWidth <= 1000) {
       setSmallScreen(true)
     }
+    handleRequest()
 
   }, [])
 
@@ -32,30 +34,48 @@ function App() {
 
   window.addEventListener('resize', widthChanged)
 
-  let dir_paths = ['public-images/Satoru Gojou/Level0.jpg',
-    'public-images/Satoru Gojou/Level1.jpg',
-    'public-images/Satoru Gojou/Level2.jpg',
-    'public-images/Satoru Gojou/Level3.jpg',
-    'public-images/Satoru Gojou/Level4.jpg',
-    'public-images/Satoru Gojou/Level5.jpg']
+
+  const [imageStrings, setImageStrings] = useState([])
+  const [options, setOptions] = useState([])
+  const [answer, setAnswer] = useState("")
+
+
 
   const [imageIdx, setImageIdx] = useState(0)
 
   function handleTimer() {
     let idx = 1;
+
     let changeInterval = setInterval(() => {
       setImageIdx(idx)
       idx++;
-      if (idx > 5){
+      if (idx > 5) {
         clearInterval(changeInterval)
       }
-
       
-      
-
     }, 1000)
 
   }
+
+  function handleRequest() {
+    axios({
+      method: 'get',
+      url: 'https://vedaantv.pythonanywhere.com/generate',  
+    })
+      .then((res) => {
+        // console.log(res.data);
+        setImageStrings(res.data.image_strings)
+        setAnswer(res.data.answer)
+        setOptions(res.data.options)
+      })
+      .catch((err) => {
+        
+        console.log(err)
+      });
+
+  }
+
+
 
 
 
@@ -71,19 +91,21 @@ function App() {
           <Row className=" mt-5">
             <Col lg="6" className="text-center d-flex flex-column align-items-center">
               <h4 className="mb-3">Who is this character?</h4>
-              <Image rounded src={dir_paths[imageIdx]} className={smallScreen ? "mb-4 dims-small" : "mb-4 dims-rel"} />
+              <Image rounded src={`data:image/jpg;base64,${imageStrings[imageIdx]}`} className={smallScreen ? "mb-4 dims-small" : "mb-4 dims-rel"} />
               <h4>Score: <span className="text-blue fw-700">0</span></h4>
               <Button onClick={handleTimer}>Test Timer</Button>
 
             </Col>
             <Col lg="6" className="mt-3 text-center d-flex flex-column align-items-center ">
               <div className="align-self-center">
-                <AnswerButton name="Satoru Gojou" />
-                <AnswerButton name="Izuku Midoriya" />
-                <AnswerButton name="Goku Son" />
-                <AnswerButton name="Gon Freecs" />
+                <AnswerButton name={options[0]} correct={answer} />
+                <AnswerButton name={options[1]} correct={answer}/>
+                <AnswerButton name={options[2]} correct={answer}/>
+                <AnswerButton name={options[3]} correct={answer}/>
 
               </div>
+              <Button onClick={handleRequest}>Test</Button>
+              
 
             </Col>
           </Row>
