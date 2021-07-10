b6fe42cc1fa6ad379247da6ba671782e980dea9a
TSharvananthan <thanussian@gmail.com>
2021-07-10 15:15:21 -0400

    added timer component

---
+++ frontend/src/components/Timer.js
@@ -0,0 +1,5 @@
+import { React, useState, useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import '../css/app.css'
+import SmallScreenContext from './SmallScreenContext';
+import axios from 'axios';
